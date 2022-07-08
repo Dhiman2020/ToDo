752bfaf8f95de585eb3b091fb9d6707d0449c642
Dhiman2020 <dhimankumbhakar0@gmail.com>
2022-07-08 20:45:11 +0530

    table

---
+++ AntDTableCRUD-master/src/App.js
@@ -0,0 +1,160 @@
+import "antd/dist/antd.css";
+import "./App.css";
+import React from 'react';
+import { Button, Table, Modal, Input } from "antd";
+import { useState } from "react";
+import { EditOutlined, DeleteOutlined } from "@ant-design/icons";
+
+let randomNumber=0;
+function App() {
+  const state={
+    curDT : new Date().toLocaleString(),
+  };
+  const [isEditing, setIsEditing] = useState(false);
+  const [editingStudent, setEditingStudent] = useState(null);
+  const [dataSource, setDataSource] = useState([]);
+  const columns = [
+     {
+       key: "1",
+       title: "ID",
+       dataIndex: "id",
+     },
+    {
+      key: "2",
+      title: "Name",
+      dataIndex: "name",
+    },
+    {
+      key: "3",
+      title: "Email",
+      dataIndex: "email",
+    },
+    {
+      key: "4",
+      title: "Address",
+      dataIndex: "address",
+    },
+    {
+      key: "5",
+      title: "Actions",
+      render: (record) => {
+        return (
+          <>
+            <EditOutlined
+              onClick={() => {
+                onEditStudent(record);
+              }}
+            />
+            <DeleteOutlined
+              onClick={() => {
+                onDeleteStudent(record);
+              }}
+              style={{ color: "red", marginLeft: 12 }}
+            />
+          </>
+        );
+      },
+    },
+    {
+      key: "6",
+      title: "Created",
+      dataIndex: "created",
+    },
+  ];
+
+  const onAddStudent = () => {
+    randomNumber = randomNumber+1;
+    const newStudent = {
+       id: randomNumber,
+       name: "",
+       email: "",
+       address: "",
+      created: state.curDT,
+    };
+    setDataSource((pre) => {
+      return [...pre, newStudent];
+    });
+  };
+  const onDeleteStudent = (record) => {
+    Modal.confirm({
+      title: "Are you sure, you want to delete this student record?",
+      okText: "Yes",
+      okType: "danger",
+      onOk: () => {
+        setDataSource((pre) => {
+          return pre.filter((student) => student.id !== record.id);
+        });
+      },
+    });
+  };
+  const onEditStudent = (record) => {
+    setIsEditing(true);
+    setEditingStudent({ ...record });
+  };
+  const resetEditing = () => {
+    setIsEditing(false);
+    setEditingStudent(null);
+  };
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Button onClick={onAddStudent}>Add a new Student</Button>
+        <Table columns={columns} dataSource={dataSource}>
+          
+        </Table>
+        <Modal
+          title="Edit Student"
+          visible={isEditing}
+          okText="Save"
+          onCancel={() => {
+            resetEditing();
+          }}
+          onOk={() => {
+            setDataSource((pre) => {
+              return pre.map((student) => {
+                if (student.id === editingStudent.id) {
+                  return editingStudent;
+                } else {
+                  return student;
+                }
+              });
+            });
+            resetEditing();
+          }}
+        >
+          <h5>Name:</h5>
+          <Input
+            value={editingStudent?.name}
+            onChange={(e) => {
+              setEditingStudent((pre) => {
+                return { ...pre, name: e.target.value };
+              });
+            }}
+          />
+          <br/>
+          <h5>Email:</h5>
+          <Input
+            value={editingStudent?.email}
+            onChange={(e) => {
+              setEditingStudent((pre) => {
+                return { ...pre, email: e.target.value };
+              });
+            }}
+          />
+          <br/>
+          <h5>Address:</h5>
+          <Input
+            value={editingStudent?.address}
+            onChange={(e) => {
+              setEditingStudent((pre) => {
+                return { ...pre, address: e.target.value };
+              });
+            }}
+          />
+        </Modal>
+      </header>
+    </div>
+  );
+}
+
+export default App;
